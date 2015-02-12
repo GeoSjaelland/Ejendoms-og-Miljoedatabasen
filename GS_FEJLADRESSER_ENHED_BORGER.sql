84903712eb76418eecbf3d7a5c2f6f71d03e70b0
Søren Breddam <widepond@gmail.com>
2015-02-12 16:17:47 +0100

    Opdatering
    Tilføjelse af tidligste indflytningsdato samt etage/side-dørnummer

--- GS_FEJLADRESSER_ENHED_BORGER.sql
+++ GS_FEJLADRESSER_ENHED_BORGER.sql
@@ -1,14 +1,26 @@
-SELECT DISTINCT 
-				GS_BORGER.Adresse, 
-				GS_BORGER.SUPBYNAVN, 
-				GS_BORGER.POSTNR, 
-				GS_BORGER.POSTBYNAVN,
-				GS_BBREnhed.Boligtypekode, 
-				GS_BORGER.ADRESSELINK_ID, 
-				GS_BORGER.ADRESSE_ID, 
+SELECT
+				MIN(DISTINCT GS_BORGER.Tidsstempling_tilflytning) AS Tidligste_indflytning,
+				GS_BORGER.Adresse,
+				GS_BORGER.Husnummer_bogstav,
+				GS_BORGER.Etage,
+				GS_BORGER.Side,
+				GS_BORGER.Supplerende_bynavn,
+				GS_BORGER.Postdistrikt,
+				GS_BORGER.ADRESSELINK_ID,
+				GS_BORGER.ADRESSE_ID,
 				GS_BORGER.UDVadresseID
-FROM 
+FROM
 				GS_BBREnhed RIGHT OUTER JOIN
 				GS_BORGER ON GS_BBREnhed.UDVadresseID = GS_BORGER.UDVadresseID
-WHERE (GS_BBREnhed.Boligtypekode < '0') OR
-				(GS_BBREnhed.Boligtypekode IS NULL)
+WHERE
+				(GS_BBREnhed.Boligtypekode < '1') OR (GS_BBREnhed.Boligtypekode IS NULL)
+GROUP BY
+				GS_BORGER.Adresse,
+				GS_BORGER.Supplerende_bynavn,
+				GS_BORGER.Etage,
+				GS_BORGER.Side,
+				GS_BORGER.Postdistrikt,
+				GS_BORGER.ADRESSELINK_ID,
+				GS_BORGER.ADRESSE_ID,
+				GS_BORGER.UDVadresseID,
+				GS_BORGER.Husnummer_bogstav
