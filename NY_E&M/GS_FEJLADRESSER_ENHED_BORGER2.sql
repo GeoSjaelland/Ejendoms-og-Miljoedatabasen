e7daa976cc649dcf98305f11f1933f259d9234b8
Søren Breddam <widepond@gmail.com>
2015-05-22 12:29:00 +0200

    Ny E&M
    Views til ny E&M, som er klargjort til implementering af Datafordeleren.
    
    Der er nogle ændringer i de bagvedliggende tabeller, hvorfor det er
    nødvendigt at ændre på standardviews. Man kan skelne de nye views fra de
    gamle ved det efterfølgende 2-tal.

---
+++ NY_E&M/GS_FEJLADRESSER_ENHED_BORGER2.sql
@@ -0,0 +1,27 @@
+SELECT DISTINCT
+				MIN(GS_BORGER.Tidsstempling_tilflytning) AS Tidligste_indflytning,
+				CONVERT(VARCHAR, GS_BORGER.Adresse) AS Adresse,
+				GS_BORGER.Husnummer_bogstav,
+				GS_BORGER.Etage,
+				GS_BORGER.Side,
+				GS_BORGER.Supplerende_bynavn,
+				CONVERT(VARCHAR, GS_BORGER.Postdistrikt) AS Postdistrikt,
+				CONVERT(VARCHAR, GS_BORGER.ADRESSELINK_ID) AS ADRESSELINK_ID,
+				CONVERT(VARCHAR, GS_BORGER.ADRESSE_ID) AS ADRESSE_ID,
+				CONVERT(VARCHAR, GS_BORGER.UDVadresseID) AS UDVadresseID
+FROM
+				GS_BBREnhed RIGHT OUTER JOIN
+				GS_BORGER ON GS_BBREnhed.UDVadresseID = GS_BORGER.UDVadresseID
+WHERE
+				(GS_BBREnhed.Boligtypekode < '1') OR (GS_BBREnhed.Boligtypekode IS NULL)
+GROUP BY
+				CONVERT(VARCHAR, GS_BORGER.Adresse),
+				GS_BORGER.Supplerende_bynavn,
+				GS_BORGER.Husnummer_bogstav,
+				GS_BORGER.Etage,
+				GS_BORGER.Side,
+				CONVERT(VARCHAR, GS_BORGER.Postdistrikt),
+				CONVERT(VARCHAR, GS_BORGER.ADRESSELINK_ID),
+				CONVERT(VARCHAR, GS_BORGER.ADRESSE_ID),
+				CONVERT(VARCHAR, GS_BORGER.UDVadresseID),
+				CONVERT(VARCHAR, GS_BORGER.Husnummer_bogstav)
